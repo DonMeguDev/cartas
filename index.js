4e6946e1caaf1d58f4d9bd7dcdf545d36a684ea1
Alexis Vergara Quijada <31351178+DonMeguDev@users.noreply.github.com>
2023-11-10 15:42:08 -0300

    Initial

---
+++ index.js
@@ -0,0 +1,57 @@
+//Elementos Nico
+const puntajeNico = document.getElementById('puntajeNico');
+const botonMenosNico = document.getElementById('botonMenosNico');
+const botonMasNico = document.getElementById('botonMasNico');
+const totalNico = document.getElementById('totalNico');
+const dolaresNico = document.getElementById('dolaresNico');
+const botonAgregarNico = document.getElementById('botonAgregarNico');
+
+//Elementos Fran
+const puntajeFran = document.getElementById('puntajeFran');
+const botonMenosFran = document.getElementById('botonMenosFran');
+const botonMasFran = document.getElementById('botonMasFran');
+const totalFran = document.getElementById('totalFran');
+const dolaresFran = document.getElementById('dolaresFran');
+const botonAgregarFran = document.getElementById('botonAgregarFran');
+
+
+let pNico = 0;
+let pFran = 0;
+let dNico = 0;
+let dFran = 0;
+
+botonMenosNico.addEventListener('click', () => {
+    if(pNico > 0){
+        pNico--;
+    }
+    puntajeNico.innerHTML = pNico;
+});
+
+botonMasNico.addEventListener('click', () => {
+    pNico++;
+    puntajeNico.innerHTML = pNico;
+});
+
+botonMenosFran.addEventListener('click', () => {
+    if(pFran > 0){
+        pFran--;
+    }
+    puntajeFran.innerHTML = pFran;
+});
+
+botonMasFran.addEventListener('click', () => {
+    pFran++;
+    puntajeFran.innerHTML = pFran;
+});
+
+botonAgregarNico.addEventListener('click', () => {
+    dNico = dNico + parseInt(dolaresNico.value);
+    dolaresNico.value = '';
+    totalNico.innerHTML = 'Total: ' + dNico + ' USD';
+});
+
+botonAgregarFran.addEventListener('click', () => {
+    dFran = dFran + parseInt(dolaresFran.value);
+    dolaresFran.value = '';
+    totalFran.innerHTML = 'Total: ' + dFran + ' USD';
+});
